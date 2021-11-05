47fc5b973be803edb9881b003e70c4838ca80543
prithvirajprab <pprabhu@usc.edu>
2021-11-05 12:55:13 -0700

    [[12,2,3]] color code added. Added a gitignore file configured to ignore models and plots and slurm output files.

--- source/pprabhu_driver.py
+++ source/pprabhu_driver.py
@@ -12,7 +12,7 @@ from ray.tune import CLIReporter
 from ray.tune.schedulers import ASHAScheduler
 
 # Activation functions
-elu = nn.ELU # Exponential linear function
+elu = nn.ELU() # Exponential linear function
 softmax = nn.Softmax(dim=0) # softmax(x_i) = \exp(x_i) / (\sum_j \exp(x_j))
 tanh = nn.Tanh()
 relu = nn.ReLU()
@@ -57,19 +57,39 @@ golay_log_ops = np.array([[0,1,0,1,0,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0
 					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,1,0]])
 
 
+color_12_2_3_stabs = np.array([[1,1,0,1,1,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0],
+[0,1,1,0,1,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,1,1,1,1,1,1,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,1,1,0,1,1,0, 0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,1,0,1,1, 0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 1,1,0,1,1,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 0,1,1,0,1,1,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,1,1,1,1,1,1,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,1,1,0,1,1,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,1,1,0,1,1]
+])
+
+color_12_2_3_log_ops = np.array([[1,1,1,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0],
+[1,0,0,1,0,0,1,0,0,1,0,0, 0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 1,0,0,1,0,0,1,0,0,1,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,1,1,1]
+])
+
 # Setting up the device and dataset
 
-dataset = '/project/tbrun_769/qdec/datasets/[[23,1,7]]p0_185data500000.csv'
-#dataset = '/project/tbrun_769/qdec/datasets/[[7,1,3]]p0142_data75000.csv'
+#dataset = '/project/tbrun_769/qdec/datasets/[[23,1,7]]Corrdata200000.csv'
+dataset = '/project/tbrun_769/qdec/datasets/[[7,1,3]]Corrdata50000.csv'
+#dataset = '/project/tbrun_769/qdec/datasets/[[12,2,3]]Corrdata75000.csv'
 checkpoint_dir = None
-num_samples=50
-max_num_epochs=40
+num_samples=25
+max_num_epochs=100
 gpus_per_trial=0
 
 # Defining the architecture
-#layersizes = [6, 120, 14]
-layersizes = [22,120,46]
-acts = [tanh, tanh, sigmoid]
+layersizes = [6, 14]
+#layersizes = [22,70,100,100,46]
+#layersizes = [10, 50, 100, 24]
+acts = [elu, sigmoid]
 
 num_epochs = max_num_epochs
 #learning_rate = 0.0005
@@ -78,10 +98,9 @@ num_epochs = max_num_epochs
 #trials_offset = 10
 
 config = {
-        "trials": tune.quniform(lower=35, upper=80, q=5),
-        "lr": tune.loguniform(1e-5, 1e-3),
+        "trials": tune.quniform(lower=2, upper=2, q=2),
+        "lr": tune.loguniform(5e-6, 1e-4),
 }
-
 
 # Filenames
 if sys.argv[1] == "n":
@@ -94,7 +113,6 @@ acc_filename = "/project/tbrun_769/qdec/models/acc_"+timestamp
 
 # Hyperparameters
 kwargs = {'epochs': num_epochs,
-
          # 'learningRate': learning_rate,
          # 'learningLast': learning_rate_final_epoch,
           'momentum': 0.9,
@@ -104,8 +122,8 @@ kwargs = {'epochs': num_epochs,
           'criterion': nn.BCELoss(),
           'mod_filename': mod_filename,
           'acc_filename': acc_filename,
-          'stabs': golay_stabs,
-          'log_ops': golay_log_ops,
+          'stabs': steane_stabs,
+          'log_ops': steane_log_ops,
           'layersizes': layersizes,
 	  'acts': acts,
 	  'dataset': dataset
@@ -113,11 +131,11 @@ kwargs = {'epochs': num_epochs,
 
 # Ray tune wrappers
 scheduler = ASHAScheduler(
-    metric="accuracy",
-    mode="max",
+    metric="loss",
+    mode="min",
     max_t=max_num_epochs,
-    grace_period=5,
-    reduction_factor=2)
+    grace_period=20,
+    reduction_factor=1.5)
 reporter = CLIReporter(
     # parameter_columns=["l1", "l2", "lr", "batch_size"],
     metric_columns=["loss", "accuracy", "x_log_val_epoch", "epoch"])
